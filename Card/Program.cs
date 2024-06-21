0b1a4f597b48fabc31f195162a7f6750cf3918e5
S-JOBI23-058\student <231064@st.yoshida-g.ac.jp>
2024-06-21 09:24:18 +0900

    1～4の数字を入力するフォームを4つ用意する。　＃1

--- Card/Program.cs
+++ Card/Program.cs
@@ -8,8 +8,21 @@ namespace Card
 {
     internal class Program
     {
+        const int MAX_CARD = 4;
+
         static void Main(string[] args)
         {
+            int[] cards = new int[MAX_CARD];
+
+            for (int i = 0; i < MAX_CARD; i++)
+            {
+                Console.Write("{0}番目のカードを入力(1～4)＞",i + 1);
+                int.TryParse(Console.ReadLine(), out cards[i]);
+            }
+
+            //キー入力待ち
+            Console.WriteLine("ENTERを押して終了");
+            Console.ReadLine();
         }
     }
 }
